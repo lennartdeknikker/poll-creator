3075db0874fb350feacc2153242dd997280f44f6
lennartdeknikker <me@lenn4rt.com>
2020-04-03 09:22:49 +0200

    update open polls

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -73,7 +73,7 @@ if (createdNew) {
 
 const pollcodes = JSON.parse(localStorage.getItem('pollcodes'))
 
-if (pollcodes) {
+if (pollcodes.length > 0) {
     const list = document.createElement('ul')
     list.classList.add('polls-list')
 
